3a62fdc8915186b43bbb8db597151a7c2be0ca20
syedajafri <syed.jafri@itgfirm.com>
2017-05-23 15:44:01 -0400

    Added examples

--- demo/src/app/components/select/multiple-demo.ts
+++ demo/src/app/components/select/multiple-demo.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'multiple-demo',
@@ -18,6 +19,7 @@ export class MultipleDemoComponent {
   private value:any = ['Athens'];
   private _disabledV:string = '0';
   private disabled:boolean = false;
+  private control: FormControl = new FormControl('', [Validators.required]);
 
   private get disabledV():string {
     return this._disabledV;
--- demo/src/app/components/select/single-demo.ts
+++ demo/src/app/components/select/single-demo.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'single-demo',
@@ -19,6 +20,7 @@ export class SingleDemoComponent {
   private value:any = {};
   private _disabledV:string = '0';
   private disabled:boolean = false;
+  private singleControl:FormControl = new FormControl('', [Validators.required]);
 
   private get disabledV():string {
     return this._disabledV;
@@ -30,6 +32,7 @@ export class SingleDemoComponent {
   }
 
   public selected(value:any):void {
+    debugger;
     console.log('Selected value is: ', value);
   }
 
